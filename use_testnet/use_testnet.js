b1957d9bfbc83552b3fc1883a30e39cd3d99d44b
Martin Lees <drov0@users.noreply.github.com>
2017-12-21 18:41:39 +0100

    Initial commit, with a js snippet example

---
+++ use_testnet/use_testnet.js
@@ -0,0 +1,16 @@
+var steem = require('steem');
+
+var testnet = true; // set to true if you want to use the testnet
+
+if (testnet)
+{
+    steem.api.setOptions({ url: 'wss://testnet.steem.vc',address_prefix:'STX',chain_id: '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673' });
+    steem.config.set('websocket','wss://testnet.steem.vc')
+    steem.config.set('address_prefix', 'STX')
+    steem.config.set('chain_id', '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673')
+}
+
+
+steem.api.getAccounts(['foo'], function(err, response){
+    console.log(response[0].balance); // Should be 0.000 STEEM for the mainnet and 1M+ for the testnet
+});
