a348098a6921d16b76dce9e05a7934a42a204cb5
Martin Lees <drov0@users.noreply.github.com>
2018-01-06 16:48:23 +0100

    adds the voting power snippet

---
+++ steemjs/voting_power/voting_power.js
@@ -0,0 +1,19 @@
+var steem = require('steem');
+
+/**
+ * Gets the voting power of an account. code mostly by stoodkev
+ * @param {String} account - account of whom we want to check the steem power
+ * @param {String} callback - callback which will have the voting power as parameter
+ */
+function getvotingpower(account, callback) {
+    steem.api.getAccounts([account], function (err, response) {
+        var secondsago = (new Date - new Date(response[0].last_vote_time + "Z")) / 1000;
+        var vpow = response[0].voting_power + (10000 * secondsago / 432000);
+        vpow = Math.min(vpow / 100, 100).toFixed(2);
+        callback(vpow);
+    });
+}
+// example
+getvotingpower("howo", function (vpower) {
+    console.log(vpower)
+});
