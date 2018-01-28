a74884e87176cadc34158b43ce8e27b12217c21c
tdreid <trevor.d.reid@gmail.com>
2018-01-28 16:52:33 +0000

    Remove jsdoc for unused parameter 'url'

--- steemjs/vote/vote.js
+++ steemjs/vote/vote.js
@@ -5,7 +5,6 @@ var steem = require('steem');
  * @param {String} username - username of the voter account
  * @param {String} password - password of the voter account
  * @param {String} author - Author of the post
- * @param {String} url - permlink of the post eg :
  * @param {String} permlink - permanent link of the post to comment to. eg : https://steemit.com/programming/@howo/introducting-steemsnippets the permlink is "introducting-steemsnippets"
  * @param {int} weight - Power of the vote, can range from -10000 to 10000, 10000 equals a 100% upvote. -10000 equals a 100% flag.
  */
