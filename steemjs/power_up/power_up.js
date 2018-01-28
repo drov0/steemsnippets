c93767addb2a6112257d18572e66235319d452ef
tdreid <trevor.d.reid@gmail.com>
2018-01-28 15:48:48 +0000

    Match description to snippet

--- steemjs/power_up/power_up.js
+++ steemjs/power_up/power_up.js
@@ -1,7 +1,7 @@
 var steem = require('steem');
 
 /**
- * Creates an account, note that almost no validation is done.
+ * Powers up tokens into STEEM Power
  * @param {String} username - username of the account
  * @param {String} Activekey - Private active key of the account
  * @param {String} amount - Amount of tokens that you want to power up, note that it must be written like this : x.xxx unit eg : '1.265 STEEM' or '0.010 STEEM'
