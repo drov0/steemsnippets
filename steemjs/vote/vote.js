6835746be749840cee27021de7856fbd832201cc
Martin Lees <drov0@users.noreply.github.com>
2018-04-23 12:13:20 +0200

    Remove password to wif from voting

--- steemjs/vote/vote.js
+++ steemjs/vote/vote.js
@@ -3,18 +3,17 @@ steem.api.setOptions({url: 'https://api.steemit.com'});
 /**
  * Casts a vote.
  * @param {String} username - username of the voter account
- * @param {String} password - password of the voter account
+ * @param {String} postingkey - posting key of the voter account
  * @param {String} author - Author of the post
  * @param {String} permlink - permanent link of the post to comment to. eg : https://steemit.com/programming/@howo/introducting-steemsnippets the permlink is "introducting-steemsnippets"
  * @param {int} weight - Power of the vote, can range from -10000 to 10000, 10000 equals a 100% upvote. -10000 equals a 100% flag.
  */
-function vote(username, password, author, permlink, weight)
+function vote(username, postingkey, author, permlink, weight)
 {
-    var wif = steem.auth.toWif(username, password, 'posting');
-    steem.broadcast.vote(wif, username, author, permlink, weight, function(err, result) {
+    steem.broadcast.vote(postingkey, username, author, permlink, weight, function(err, result) {
         console.log(err, result);
     });
 }
 
 // example : give a 100% upvote to the post https://steemit.com/programming/@howo/introducting-steemsnippets
-vote("username", "password", "howo", "introducting-steemsnippets", 10000);
+vote("username", "postingkey", "howo", "introducting-steemsnippets", 10000);
