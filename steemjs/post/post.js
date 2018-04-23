7f4c72e7b14f3ed826dcc86a3897bb54ff6519d0
Martin Lees <drov0@users.noreply.github.com>
2018-04-23 12:47:52 +0200

    Remove the password to wif from posting

--- steemjs/post/post.js
+++ steemjs/post/post.js
@@ -3,20 +3,19 @@ steem.api.setOptions({url: 'https://api.steemit.com'});
 /**
  * Posts an article to the steem blockchain
  * @param {String} username - username of the account
- * @param {String} password - password of the account
+ * @param {String} postingkey - private posting key of the account
  * @param {String} main_tag - The main tag for the post
  * @param {String} title - Title of the post
  * @param {String} body - body (content) of the post.
  * @param {object} [jsonMetadata] - dictionnary with additional tags, app name, etc,
  * @param {String} [permlink] - permanent link, by default it's the date + -post. eg : 20171237t122520625z-post
  */
-function post(username, password, main_tag, title, body, jsonMetadata, permlink) {
-    var wif = steem.auth.toWif(username, password, 'posting');
+function post(username, postingkey, main_tag, title, body, jsonMetadata, permlink) {
     // By default permlink will be the date
     permlink = permlink || new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
     jsonMetadata = jsonMetadata || {};
 
-    steem.broadcast.comment(wif, '',  main_tag, username, permlink + '-post', title, body, jsonMetadata, function (err, result) {
+    steem.broadcast.comment(postingkey, '',  main_tag, username, permlink + '-post', title, body, jsonMetadata, function (err, result) {
         console.log(err, result);
     });
 }
