b3d047073a221e375659c5b616532280f74402e2
Martin Lees <drov0@users.noreply.github.com>
2018-12-07 04:42:55 +0100

    Get_all_comments working without get_state

--- dsteem/get_all_comments/get_all_comments.js
+++ dsteem/get_all_comments/get_all_comments.js
@@ -4,29 +4,83 @@ const dsteem = require('dsteem');
 
 const client = new dsteem.Client('https://api.steemit.com');
 
-const steem_bots = [
-    "upme",
-    "postpromoter",
-    "boomerang",
-    "coin.info",
-    "arcange",
-    "steemcleaners",
-    "bid4joy",
-    "onlyprofitbot",
-    "brupvoter",
-    "upmewhale",
-    "joeparys",
-    "booster",
-    "emperorofnaps",
-    "spydo",
-    "dailyupvotes",
-    "minnowvotes",
-    "minnowbooster",
-    "oceanwhale",
-    "steemitboard",
-    "cheetah"
-];
+function build_get_replies(reply)
+{
+    return new Promise(async resolve => {
 
+        console.log(reply.depth);
+
+        reply.active_votes =  await client.database.call("get_active_votes", [reply.author, reply.permlink]);
+
+        const userTimezoneOffset = new Date().getTimezoneOffset() * 60000;
+        const chosenDate = moment.unix(new Date(reply.created).getTime() / 1000);
+        reply.unix = chosenDate.unix();
+        reply.date = chosenDate.from(new Date().getTime()  + userTimezoneOffset);
+
+        reply.reputation = utils.repLog10(reply.author_reputation);
+
+        const total_payout =
+            parseFloat(reply.pending_payout_value) +
+            parseFloat(reply.total_payout_value) +
+            parseFloat(reply.curator_payout_value);
+
+        reply.total_payout = (Math.floor(parseFloat(total_payout) * 100) / 100);
+
+        reply.upvotes = reply.active_votes.length;
+
+        const voteRshares = reply.active_votes.reduce((a, b) => a + parseFloat(b.rshares), 0);
+
+        const ratio = (voteRshares === 0 ? 0 : reply.total_payout / voteRshares);
+
+        reply.active_votes.map((vote) => {
+            vote.value = Math.floor((vote.rshares * ratio)*100)/100;
+            if (vote.percent === 0)
+                reply.active_votes.splice(reply.active_votes.indexOf(vote), 1)
+        });
+
+        reply.active_votes.sort(utils.compare_votes);
+
+
+        if (reply.children > 0)
+        {
+            reply.replies = await client.database.call("get_content_replies", [reply.author, reply.permlink]);
+
+
+            for (let i = 0; i < reply.replies.length; i++) {
+                reply.replies[i] = await build_get_replies(reply.replies[i]);
+            }
+
+            return resolve(reply);
+        } else
+        {
+            return resolve(reply);
+        }
+
+
+    })
+}
+
+function build_comment_data(author, permlink)
+{
+    return new Promise(async resolve => {
+        let post = await client.database.call("get_content", [author, permlink]).catch(function (err) {
+            console.error(err);
+            });
+
+        if (post['root_permlink'] === "" && post['root_author'] === "" )
+            return resolve(post);
+
+        post.replies = await client.database.call("get_content_replies", [author, permlink]).catch(function (err) {
+                return resolve({error: err.message, comments: ""});
+            });
+
+        for (let i = 0; i < post.replies.length; i++) {
+            post.replies[i] = await build_get_replies(post.replies[i]);
+        }
+
+        return resolve(post);
+    });
+}
 
 
 function get_all_comments(author, permlink)
@@ -36,112 +90,21 @@ function get_all_comments(author, permlink)
         if (author === "" || permlink === "")
             return resolve({error:"Post not found", comments : ""});
 
-        const post = await client.database.getState("a/@"+author+"/"+permlink).catch(function (err) {
-            return resolve({error : err.message, comments : ""});
-        });
-
-        if (post.error !== "")
-            return resolve({error : post.error, comments : ""});
+        const post = await build_comment_data(author, permlink);
 
         if (post['root_permlink'] === "" && post['root_author'] === "" )
             return resolve({error:"Post not found", comments : ""});
 
-        let comment_list = post.content;
-        let comments_ordered = [];
-        let author_list = new Set([]); // will be used later to query the blockchain for user avatars
-
-        for (const comment_id in comment_list) {
-
-            let comment = comment_list[comment_id];
-
-            if (comment.depth !== 0)
-                continue;
-
-            comments_ordered.push(await get_replies(comment, comment_list, author_list));
-        }
-
-        return resolve({comments:comments_ordered});
+        return resolve({comments : post});
     });
 }
-
-
-
-
-
-async function get_replies(comment, comment_list, author_list)
-{
-    return new Promise(async resolve => {
-        author_list.add(comment.author);
-
-        const userTimezoneOffset = new Date().getTimezoneOffset() * 60000;
-        const chosenDate = moment.unix(new Date(comment.created).getTime() / 1000);
-        comment.unix = chosenDate.unix();
-        comment.date = chosenDate.from(new Date().getTime()  + userTimezoneOffset);
-
-        comment.reputation = utils.repLog10(comment.author_reputation);
-
-        const total_payout =
-            parseFloat(comment.pending_payout_value) +
-            parseFloat(comment.total_payout_value) +
-            parseFloat(comment.curator_payout_value);
-
-        comment.total_payout = (Math.floor(parseFloat(total_payout) * 100) / 100);
-
-        //const vote_data = count_votes(comment.active_votes);
-        comment.upvotes = comment.active_votes.length;
-
-        const voteRshares = comment.active_votes.reduce((a, b) => a + parseFloat(b.rshares), 0);
-
-        const ratio = (voteRshares === 0 ? 0 : comment.total_payout / voteRshares);
-
-        comment.active_votes.map((vote) => {
-            vote.value = Math.floor((vote.rshares * ratio)*100)/100;
-            if (vote.percent === 0)
-                comment.active_votes.splice(comment.active_votes.indexOf(vote), 1)
-        });
-
-        comment.active_votes.sort(utils.compare_votes);
-
-
-
-        for (let i = 0; i < comment.replies.length; i++) {
-            let reply = comment_list[comment.replies[i]];
-
-
-            if (steem_bots.indexOf(reply.author) !== -1)
-                continue;
-
-            reply.body = await utils.parse_text(reply.body);
-            comment.replies[i] = await get_replies(reply, comment_list, author_list);
-        }
-
-
-        return resolve(comment);
-    });
-}
-
-function get_comment_nb(comment)
-{
-    let comment_nb = 0;
-
-    if (comment.total_payout === undefined) {
-        return comment_nb - 1;
-    }
-
-    comment_nb += comment.replies.length;
-
-    for (let i = 0; i < comment.replies.length; i++) {
-        let reply = comment.replies[i];
-        comment_nb += get_comment_nb(reply);
-    }
-
-    return comment_nb;
-}
-
 
 async function test() {
     var data = await get_all_comments("petanque", "testcomments-xylmwuwi6p");
-    console.log(data);
+
+    if (data.comments === "" && data.error !== undefined) {
+        console.error(data.error);
+     }
 }
 
-test()
+test();
